f4f42f14c148b39c81979cc76040258292843c6a
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-01-23 14:18:56 -0500

    passed tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, poss_matches):
+        matches = []
+        for word in poss_matches:
+            if(sorted([letter for letter in word]) == sorted([letter for letter in self.word])):
+                matches.append(word)
+
+        return matches
